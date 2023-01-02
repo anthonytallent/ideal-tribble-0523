10ee2085709b2886782007b10fcb944556f71766
Anthony Blackwell Tallent <anthonytallent567@gmail.com>
2023-01-02 09:58:36 -0800

    finish us 2

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "index" do
   before(:each) do
-    universal = Studio.create!(name: "Universal Studios", location: "LA")
-    limo = Studio.create!(name: "Limo Productions", location: "Glendale")
+    @universal = Studio.create!(name: "Universal Studios", location: "LA")
+    @limo = Studio.create!(name: "Limo Productions", location: "Glendale")
 
-    mummy = universal.movies.create!(title: "The Mummy", creation_year: "1999", genre: "Action")
-    legally_blonde = universal.movies.create!(title: "Legally Blonde", creation_year: "2000", genre: "comedy")
-    the_grinch = limo.movies.create!(title: "The Grinch", creation_year: "2001", genre: "Holiday")
+    @mummy = @universal.movies.create!(title: "The Mummy", creation_year: "1999", genre: "Action")
+    @legally_blonde = @universal.movies.create!(title: "Legally Blonde", creation_year: "2000", genre: "comedy")
+    @the_grinch = @limo.movies.create!(title: "The Grinch", creation_year: "2001", genre: "Holiday")
 
-    jim = the_grinch.actors.create!(name: "Jim Carrey", age: "30")
-    reese = legally_blonde.actors.create!(name: "Reese Witherspoon", age: "32")
-    brendan = mummy.actors.create!(name: "Brendan Frazier", age: "33")
-    cindy = the_grinch.actors.create!(name: "Cindy Lou-Who", age: "8")
+    @jim = @the_grinch.actors.create!(name: "Jim Carrey", age: "30")
+    @reese = @legally_blonde.actors.create!(name: "Reese Witherspoon", age: "32")
+    @brendan = @mummy.actors.create!(name: "Brendan Frazier", age: "33")
+    @cindy = @the_grinch.actors.create!(name: "Cindy Lou-Who", age: "8")
   end
   it 'lists all studios and their name and location' do
     visit '/studios/index'
